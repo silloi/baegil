b26ffea33138158a0f17d99191e1e814b46a01eb
silloi <silloi1993@gmail.com>
2020-10-03 16:40:57 +0900

    feat: implement calendar

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,41 @@
       <v-card-title>
         記念日をカレンダーで計算
       </v-card-title>
-      <v-calendar />
+      <v-sheet
+        tile
+        class="d-flex"
+      >
+        <v-btn
+          icon
+          class="ma-2"
+          @click="$refs.calendar.prev()"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-spacer />
+        <v-toolbar-title v-if="$refs.calendar">
+          {{ $refs.calendar.title }}
+        </v-toolbar-title>
+        <v-spacer />
+        <v-btn
+          icon
+          class="ma-2"
+          @click="$refs.calendar.next()"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-sheet>
+      <v-sheet height="420">
+        <v-calendar
+          ref="calendar"
+          v-model="value"
+          locale="ja-jp"
+          :day-format="timestamp => new Date(timestamp.date).getDate()"
+          :month-format="timestamp => (new Date(timestamp.date).getMonth() + 1) + ' /'"
+          :events="events"
+          @click:date="selectDay"
+        />
+      </v-sheet>
       <v-card>
         <v-card-title class="headline">
           記念日
@@ -49,6 +83,57 @@
 <script>
 
 export default {
-  components: {}
+  data: () => ({
+    focus: new Date(),
+    value: '',
+    events: [],
+    type: 'month',
+    typeToLabel: {
+      month: '月',
+      week: '週',
+      day: '日'
+    }
+  }),
+  mounted () {
+    this.setDay(new Date())
+  },
+  methods: {
+    setDay (date) {
+      const calculateDayAfter = day => new Date(Date.parse(date) + 1000 * 60 * 60 * 24 * day)
+
+      const numbersMemorial = [
+        10,
+        20,
+        50,
+        100,
+        200,
+        500
+      ]
+
+      const datesMemorial = numbersMemorial.map((number) => {
+        return {
+          number,
+          date: calculateDayAfter(number)
+        }
+      })
+
+      const events = []
+
+      datesMemorial.map((date) => {
+        events.push({
+          name: `${date.number}日目`,
+          start: date.date,
+          end: date.date
+        })
+      })
+
+      this.events = events
+    },
+    selectDay ({ date }) {
+      const dateDate = new Date(`${date}T00:00:00`)
+
+      this.setDay(dateDate)
+    }
+  }
 }
 </script>
