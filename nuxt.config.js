84b38b41039eff6c634e20c18183b9ad95d5beac
silloi <silloi1993@gmail.com>
2020-10-06 01:46:26 +0900

    feat: add canonical url

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: 'https://100日記念日.com' }
     ]
   },
 
